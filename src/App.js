7c4d706d651f512def5ff81af0b6ca368289b82b
nokaur <71750548+nokaur@users.noreply.github.com>
2020-10-07 13:55:24 +0530

    Changes-3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import Reception from "./Hotel_Management/Reception/ReceptionIndex";
 import "./styles.css";
 
 import Login from "./Hotel_Management/Login";
 
 class App extends Component {
   state = {
-    Role: ""
+    Role: "",
+    Redirect: ""
   };
   OnSubmit = (Username) => {
     if (Username === "owner") {
     } else if (Username === "manager") {
     } else if (Username === "reception") {
+      this.setState({
+        Redirect: "./Hotel_Management/Reception/ReceptionIndex"
+      });
     }
   };
   render() {
@@ -21,5 +27,16 @@ class App extends Component {
     );
   }
 }
-
 export default App;
+
+// }
+// import React, { Component } from "react";
+
+// class Reception extends Component {
+//   render() {
+//     alert("index");
+//     return <div>hello</div>;
+//   }
+// }
+
+// export default Reception;
